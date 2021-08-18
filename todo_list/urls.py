5cd6dfd0ae897ecb0c3248c3766b9705bff05902
Anurag Borhade <76247619+itsanurag22@users.noreply.github.com>
2021-08-18 17:34:03 +0530

    Add files via upload

--- todo_list/urls.py
+++ todo_list/urls.py
@@ -1,18 +1,18 @@
 from django.urls import path
 from django.conf.urls import url
-from .views import deleteItem, index, details, create, deleteList, updateList, deleteItem, addItem, markItem, updateItem
+from .views import delete_item, index, details, create, delete_list, update_list, delete_item, add_item, mark_item, update_item
 app_name='todo'
 urlpatterns = [
     path('', index, name='index'),
 
     path('<int:list_id>/', details, name='list_details'),
     path('create/', create, name='list_create'),
-    path('deleteL/<int:list_id>/', deleteList, name='list_delete'),
-    path('updateL/<int:list_id2>/', updateList, name='list_update'),
-    path('deleteI/<int:item_id>/', deleteItem, name='item_delete'),
-    path('addI/<int:list_id>/', addItem, name='item_add'),
-    path('markI/<int:item_id>/', markItem, name='item_mark'),
-    path('updateI/<int:item_id>/', updateItem, name='item_update')
+    path('deleteL/<int:list_id>/', delete_list, name='list_delete'),
+    path('updateL/<int:list_id2>/', update_list, name='list_update'),
+    path('deleteI/<int:item_id>/', delete_item, name='item_delete'),
+    path('addI/<int:list_id>/', add_item, name='item_add'),
+    path('markI/<int:item_id>/', mark_item, name='item_mark'),
+    path('updateI/<int:item_id>/', update_item, name='item_update')
 
 
 
